d7a8ba1e5e3f5eadcd43cfe83563131eb109def2
Alan Hollis <me@alanhollis.com>
2025-12-27 16:03:31 +0000

    Add Capacitor Android setup

---
+++ android/app/src/main/java/com/freekidstypinggames/MainActivity.java
@@ -0,0 +1,5 @@
+package com.freekidstypinggames;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- flake.nix
+++ flake.nix
@@ -17,12 +17,44 @@
     in {
       devShells = forEachSystem (system:
         let
-          pkgs = import nixpkgs { inherit system; };
+          pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfreePredicate = pkg:
+                pkgs.lib.hasPrefix "android-" (pkgs.lib.getName pkg)
+                || pkgs.lib.hasPrefix "platform" (pkgs.lib.getName pkg)
+                || pkgs.lib.hasPrefix "build-tools" (pkgs.lib.getName pkg)
+                || pkgs.lib.hasPrefix "extras-" (pkgs.lib.getName pkg)
+                || pkgs.lib.hasPrefix "google-" (pkgs.lib.getName pkg)
+                || pkgs.lib.hasPrefix "cmake" (pkgs.lib.getName pkg)
+                || pkgs.lib.hasPrefix "cmdline-tools" (pkgs.lib.getName pkg)
+                || pkgs.lib.elem (pkgs.lib.getName pkg) [
+                  "androidsdk"
+                  "platform-tools"
+                  "tools"
+                ];
+              android_sdk.accept_license = true;
+            };
+          };
+          android = pkgs.androidenv.composeAndroidPackages {
+            platformVersions = [ "34" ];
+            buildToolsVersions = [ "34.0.0" ];
+            includeNDK = false;
+            includeEmulator = false;
+            includeSystemImages = false;
+            includeSources = false;
+            includeExtras = [
+              "extras;android;m2repository"
+              "extras;google;m2repository"
+            ];
+          };
         in {
           default = pkgs.mkShell {
             buildInputs = [
               pkgs.hugo
               pkgs.nodejs
+              pkgs.jdk17
+              android.androidsdk
               (pkgs.python313.withPackages (ps: with ps; [
                 google-cloud-texttospeech
                 google-genai
@@ -31,6 +63,8 @@
             ];
             shellHook = ''
               export PATH=$PWD/node_modules/.bin:$PATH
+              export ANDROID_HOME=${android.androidsdk}/libexec/android-sdk
+              export ANDROID_SDK_ROOT=$ANDROID_HOME
               echo "Dev shell loaded with Hugo ${pkgs.hugo.version}."
             '';
           };
